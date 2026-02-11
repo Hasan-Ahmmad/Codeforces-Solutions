acfea3e555caee7fc8c9c7aff45e00c76c16ab1f
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-02-11 18:56:58 +0600

    Magical Sticks

---
+++ 1371A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        if(n%2==0)
+        {
+            cout<<n/2<<endl;
+        }
+        else cout<< n/2+1<<endl;
+    }
+    
+    return 0;
+}
