7f7bf53b724801b62348b9bf8907e41583281eaa
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-02-12 00:49:05 +0600

    Goals of Victory

---
+++ 1887A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        int sum=0;
+        for(int i=0;i<n-1;i++)
+        {
+            int a; cin>>a;
+            sum+=a;
+        }
+        cout<<sum*-1<<endl;
+    }
+    
+    return 0;
+}
