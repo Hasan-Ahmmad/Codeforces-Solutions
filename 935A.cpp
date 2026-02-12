a95d387fb1ad1ccebd96958f3970ce92ed265da2
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-02-12 17:13:53 +0600

    Fafa and his Company

---
+++ 935A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n; cin>>n;
+    int count=0;
+    for(int i=1;i<=n/2;i++)
+    {
+        if((n-i)%i==0) count++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
