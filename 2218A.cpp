5f55db6307658de3dd8c7a072bebd3ba5421c671
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-04-11 12:32:21 +0600

    The 67th Integer Problem

---
+++ 2218A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--){
+    
+        int x; cin>>x;
+        cout<<67<<endl;
+    
+    }
+    return 0;
+}
