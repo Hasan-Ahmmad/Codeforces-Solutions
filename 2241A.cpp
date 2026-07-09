681ef990ff5c83709c53369f275470196d7fbb57
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-07-09 22:41:36 +0600

    Create 2241A.cpp

---
+++ 2241A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--){
+    
+        int x,y;
+        cin>>x>>y;
+
+        if(x%y==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
