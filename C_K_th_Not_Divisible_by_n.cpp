3dfd24d0871ed2bf32b178d8da566c4da979d41a
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-08-09 00:02:23 +0600

    1352C

---
+++ C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin>>t;
+    while(t--){
+    
+        int n, k; cin>>n>>k;
+
+        cout<< k + k-1/n-1 <<endl;
+    
+    }
+    return 0;
+}
